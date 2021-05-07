af0f47e90151d785b9b910384a6d3efc48b3de62
Marcin Kaluza <mkaluz@amazon.com>
2021-05-08 00:14:55 +0100

    Updated rolling policy

--- src/main/java/com/amazonaws/services/kinesisanalytics/CustomRollingPolicy.java
+++ src/main/java/com/amazonaws/services/kinesisanalytics/CustomRollingPolicy.java
@@ -8,20 +8,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Instant;
 
 public class CustomRollingPolicy extends CheckpointRollingPolicy<StockTick, String> {
 
+    private final static int MB = 1024 * 1024;
     Logger LOG = LoggerFactory.getLogger(CustomRollingPolicy.class);
 
     @Override
     public boolean shouldRollOnEvent(PartFileInfo<String> partFileInfo, StockTick stockTick) throws IOException {
-        LOG.info("File size: {}", partFileInfo.getSize());
-        return partFileInfo.getSize() > 100 * 1024;
+
+        if(shouldRoll(partFileInfo)){
+            LOG.info("Roll on enabled - file size: {}. Bucket: {}", partFileInfo.getSize(), partFileInfo.getBucketId());
+            return true;
+        };
+
+        return false;
+    }
+
+    private boolean shouldRoll(PartFileInfo<String> partFileInfo) throws IOException {
+        return partFileInfo.getSize() > 1 * MB || partFileInfo.getLastUpdateTime() < Instant.now().minusSeconds(300).toEpochMilli();
     }
 
     @Override
     public boolean shouldRollOnProcessingTime(PartFileInfo<String> partFileInfo, long l) throws IOException {
-        LOG.info("Processing time file size: {}", partFileInfo.getSize());
-        return true;
+        if(shouldRoll(partFileInfo)){
+            LOG.info("Roll on enabled - file size: {}. Bucket: {}", partFileInfo.getSize(), partFileInfo.getBucketId());
+            return true;
+        };
     }
 }
