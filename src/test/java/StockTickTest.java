35ca111a91ffa244982dae6c1d2ec901f4754eca
Marcin Kaluza <mkaluz@amazon.com>
2021-05-10 14:29:49 +0100

    Fixed unit test

--- src/test/java/StockTickTest.java
+++ src/test/java/StockTickTest.java
@@ -12,10 +12,10 @@ public class StockTickTest {
     @Test
     public void deserializeJson() throws IOException {
         var json = "{\n" +
-                "  \"isin\": \"GB0000100767\",\n" +
-                "  \"timeStamp\": \"2021-01-04T08:05:12Z\",\n" +
-                "  \"bid\" : 12.45,\n" +
-                "  \"ask\" : 12.55\n" +
+                "  \"RIC\": \"GB0000100767\",\n" +
+                "  \"Date_Time\": \"2021-01-04T08:05:12Z\",\n" +
+                "  \"Bid_Price\" : 12.45,\n" +
+                "  \"Ask_Price\" : 12.55\n" +
                 "}";
 
         var expectedTimestamp =  new DateTime(2021, 1, 4, 8, 5, 12, 0);
